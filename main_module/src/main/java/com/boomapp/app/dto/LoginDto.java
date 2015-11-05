147dcbc8a74a82a57aa060e4b4931672939c7df9
Hamid <hr.astian@gmail.com>
2015-11-05 21:09:08 +0330

    add LoginDto

---
+++ main_module/src/main/java/com/boomapp/app/dto/LoginDto.java
@@ -0,0 +1,26 @@
+package com.boomapp.app.dto;
+
+/**
+ * Created by Hamid on 11/05/2015.
+ */
+public class LoginDto {
+
+    private String cif;
+    private String password;
+
+    public String getCif() {
+        return cif;
+    }
+
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

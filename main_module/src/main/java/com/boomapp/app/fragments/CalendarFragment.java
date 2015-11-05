b9dcfc59b920a0ce2f263735f6f2132f200d0d90
Hamid <hr.astian@gmail.com>
2015-11-05 13:46:08 +0330

    calendar click listener

--- main_module/src/main/java/com/boomapp/app/fragments/CalendarFragment.java
+++ main_module/src/main/java/com/boomapp/app/fragments/CalendarFragment.java
@@ -6,8 +6,12 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.boomapp.app.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 /**
  * Created by Hamid on 11/05/2015.
@@ -16,8 +20,17 @@ public class CalendarFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.mat_calendar,null);
+
+        MaterialCalendarView calendarView = (MaterialCalendarView) inflater.inflate(R.layout.mat_calendar, null);
+
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(MaterialCalendarView materialCalendarView, CalendarDay calendarDay, boolean b) {
+                Toast.makeText(container.getContext(), calendarDay.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        return calendarView;
     }
 }

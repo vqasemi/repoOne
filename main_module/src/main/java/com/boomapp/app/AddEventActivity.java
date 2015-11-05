52ea7fa57a5323c2c025c8775d90d1b3ee5aafdb
Hamid <hr.astian@gmail.com>
2015-11-05 19:57:45 +0330

    add event activity 2

--- main_module/src/main/java/com/boomapp/app/AddEventActivity.java
+++ main_module/src/main/java/com/boomapp/app/AddEventActivity.java
@@ -2,9 +2,12 @@ package com.boomapp.app;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * Created by Hamid on 11/05/2015.
@@ -12,14 +15,19 @@ import android.view.View;
 
 public class AddEventActivity extends Activity {
 
-    @Nullable
     @Override
-    public View onCreateView(String name, Context context, AttributeSet attrs) {
-        super.onCreateView(name, context, attrs);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_event_layout);
 
+        Spinner type = (Spinner) findViewById(R.id.spinner);
+        Button OK = (Button) findViewById(R.id.OKbtn);
 
+        OK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-
-        return null;
+            }
+        });
     }
 }

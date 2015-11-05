0234664ea94a0aa9b396c7cb10dffeae3dd54fc3
Hamid <hr.astian@gmail.com>
2015-11-05 18:17:23 +0330

    add event activity

---
+++ main_module/src/main/java/com/boomapp/app/AddEventActivity.java
@@ -0,0 +1,25 @@
+package com.boomapp.app;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Hamid on 11/05/2015.
+ */
+
+public class AddEventActivity extends Activity {
+
+    @Nullable
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        super.onCreateView(name, context, attrs);
+
+
+
+
+        return null;
+    }
+}
